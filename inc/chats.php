5234d8a8cc422827bc4c19410a989f4104052f0a
Luis Vargas <luisf55horn@gmail.com>
2019-09-22 22:38:14 -0400

    chat has been added

--- inc/chats.php
+++ inc/chats.php
@@ -1,5 +1,80 @@
-      <div class="tab-pane fade show active" id="conversaciones" role="tabpanel"  aria-labelledby="chat-tab" >
-        <h1 class="mt-4">Chats</h1>
-        <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
-        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p>
-      </div>
+      
+      
+<div class="tab-pane fade show active" id="conversaciones" role="tabpanel"  aria-labelledby="chat-tab" >
+    
+    <div class="chatContainer">
+      
+        <div class="people-list" id="people-list">
+
+          <div class="search">
+          
+          </div>
+
+          <div class="people">
+
+            <ul>
+              <li>
+                <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil">
+                <h2> Nombre </h2>
+              
+              </li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          
+          </div>
+               
+        </div>
+
+        <div class="messaging">
+          <div class="inbox_msg">
+
+            <div class="mesgs">
+              <div class="msg_history">
+                <div class="incoming_msg">
+                  <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                  <div class="received_msg">
+                    <div class="received_withd_msg">
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                      <span class="time_date"> 11:01 AM    |    June 9</span></div>
+                  </div>
+                </div>
+                <div class="outgoing_msg">
+                  <div class="sent_msg">
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                    <span class="time_date"> 11:01 AM    |    June 9</span> </div>
+                </div>
+                <div class="incoming_msg">
+                  <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                  <div class="received_msg">
+                    <div class="received_withd_msg">
+                      <p>Test, which is a new approach to have</p>
+                      <span class="time_date"> 11:01 AM    |    Yesterday</span></div>
+                  </div>
+                </div>
+                <div class="outgoing_msg">
+                  <div class="sent_msg">
+                    <p>Apollo University, Delhi, India Test</p>
+                    <span class="time_date"> 11:01 AM    |    Today</span> </div>
+                </div>
+                <div class="incoming_msg">
+                  <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                  <div class="received_msg">
+                    <div class="received_withd_msg">
+                      <p>We work directly with our designers and suppliers,
+                        and sell direct to you, which means quality, exclusive
+                        products, at a price anyone can afford.</p>
+                      <span class="time_date"> 11:01 AM    |    Today</span></div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+  
+  
+    </div>
+
+</div>
--- inc/header.php
+++ inc/header.php
@@ -8,21 +8,20 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
+              <a class="nav-link dropdown-toggle setting" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Tu cuenta <img class="settingsIcon" src="../assets/svg/settings.svg" alt="settings">
               </a>
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item setItem" href="#">
+                Estado
+                <svg class="state" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="#000000">
+                  <path d="M 12 2 C 6.48 2 2 6.48 2 12 C 2 17.52 6.48 22 12 22 C 17.52 22 22 17.52 22 12 C 22 6.48 17.52 2 12 2 z M 12 4 C 16.41 4 20 7.59 20 12 C 20 16.41 16.41 20 12 20 C 7.59 20 4 16.41 4 12 C 4 7.59 7.59 4 12 4 z M 12 6 C 8.69 6 6 8.69 6 12 C 6 15.31 8.69 18 12 18 C 15.31 18 18 15.31 18 12 C 18 8.69 15.31 6 12 6 z"/>
+                </svg>
+                </a> 
+                <a class="dropdown-item setItem" href="#">Perfil</a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
+                <a class="dropdown-item setItem" href="#">Salir</a>
               </div>
             </li>
           </ul>
